4f426a8de2710513908c20235b148e3e0211b087
bimbowande <oluwagbuyi01@gmail.com>
2025-02-26 15:50:42 +0000

    union types

--- def/union-types.ts
+++ def/union-types.ts
@@ -1,13 +1,21 @@
 //a variable or function parameter can have more than one type
 
 let uId: string | number;
-uId = 405
-uId = "user-405"
-uId = true // this will give error because boolean is not a string or number
+uId = 405;
+uId = "user-405";
+uId = true; // this will give error because boolean is not a string or number
 
 function add(a: number | string, b: number | string) {
-    if (typeof a === "string" || typeof b === "string") {
-        return a.toString() + b.toString()
-    }
-    return a + b
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+//the  function that returns a value based on the type of the input
+function returnValue(a: number | string): string | number {
+  if (typeof a === "string") {
+    return a.toString();
+  }
+  return a;
 }
